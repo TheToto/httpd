53919f7d539018f3ee3f6f7210865b2eb98986f1
Arthur Busuttil <arthur.busuttil@epita.fr>
2019-02-14 15:08:51 +0100

    [ADD][dispatch] Update dispatch func. and add file

---
+++ src/vhost/dispatcher.cc
@@ -0,0 +1,27 @@
+#include "dispatcher.hh"
+namespace http
+{
+    shared_vhost Dispatcher::operator()(Request& r)
+    {
+        const std::string& host = r.get_header("Host");
+        auto cur = vhosts_.begin();
+        for(; cur != vhosts_.end(); cur++)
+        {
+            auto conf = (*cur)->conf_get();
+            if (conf.ip_ == host)
+                break;
+            else if (conf.server_name_ == host)
+                break;
+            else if (conf.ip_port_ == host)
+                break;
+            else if (conf.server_name_port_ == host)
+                break;
+        }
+        if (cur == vhosts_.end())
+        {
+            r.set_mode("ERROR");
+            return NULL;
+        }
+        return *cur;
+    }
+}
