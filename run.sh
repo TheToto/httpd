7608e560907e670210d37353f46d5f99080ce7b7
Geraud Magne <geraud.magne@epita.fr>
2019-02-14 18:25:34 +0100

    [ARCH] ./run.sh now works multiple times properly

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+shopt -s dotglob
 
 if test ! -d build; then
     mkdir build
     ./autogen.sh
     ./configure
 else
-    mv build/* .
+    mv -f build/* . &> /dev/null
 fi
 
 make CXXFLAGS+='-O0' -j9
-mv .libs libspider.la Makefile aclocal.m4 autom4te.cache build-aux config.h config.h.in config.log config.status configure libtool m4 Makefile.in stamp-h1 build/
+mv -f .libs/ libspider.la Makefile aclocal.m4 autom4te.cache build-aux config.h config.h.in config.log config.status configure libtool m4 Makefile.in stamp-h1 build/ &> /dev/null
