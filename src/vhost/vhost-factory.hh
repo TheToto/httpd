5023c8b15c0097d52f4d2c42d95ae988e5fcee0c
Thomas Lupin <thomas.lupin@epita.fr>
2019-02-26 09:00:49 +0100

    [IPV6] Throw exeption if ip is not a ipv4 or ipv6

--- src/vhost/vhost-factory.hh
+++ src/vhost/vhost-factory.hh
@@ -8,9 +8,9 @@
 #include "events/server.hh"
 #include "socket/default-socket.hh"
 #include "vhost/dispatcher.hh"
+#include "vhost/vhost-fail.hh"
 #include "vhost/vhost-static-file.hh"
 #include "vhost/vhost.hh"
-#include "vhost/vhost-fail.hh"
 namespace http
 {
     /**
@@ -41,8 +41,11 @@ namespace http
             server.sin_family = AF_INET;
 
             // Ues AF_INET6 for IPv6
-            if (inet_pton(AF_INET6, conf.ip_.c_str(), &(server.sin_addr)) == 0)
-                inet_pton(AF_INET, conf.ip_.c_str(), &(server.sin_addr));
+            if (inet_pton(AF_INET6, conf.ip_.c_str(), &(server.sin_addr)) == 0
+                && inet_pton(AF_INET, conf.ip_.c_str(), &(server.sin_addr))
+                    == 0)
+                throw new std::runtime_error("Can't assign this ip : "
+                                             + conf.ip_);
             server.sin_port = htons(conf.port_);
 
             sock->bind((sockaddr*)&server, sizeof(server));
