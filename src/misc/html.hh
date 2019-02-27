72f4a4cee6fdf1e924bdec50d6da8dc362c5faba
Thomas Lupin <thomas.lupin@epita.fr>
2019-02-27 22:20:42 +0100

    [HTML] Better errors html

---
+++ src/misc/html.hh
@@ -0,0 +1,35 @@
+/**
+ * \file misc/html.cc
+ * \brief Generate HTML errors
+ */
+#include <string>
+
+#include "request/types.hh"
+
+namespace misc
+{
+    class Html
+    {
+    public:
+        static std::string generate_html(std::string title, std::string body)
+        {
+            std::string html;
+            html = "<!DOCTYPE html><html>"
+            "<head><meta charset=utf-8><title>" + title + "</title>"
+            "<style></style>"
+            "</head><body>" + body + "</body></html>";
+            return html;
+        }
+
+        static std::string generate_error(http::STATUS_CODE& status)
+        {
+            if (status == http::STATUS_CODE::NOT_FOUND)
+                return generate_html("404 Not Found", "<h3>404 error</h3><p>The requested URL was not found on this server.</p>");
+            if (status == http::STATUS_CODE::FORBIDDEN)
+                return generate_html("403 Forbidden", "<h3>403 error</h3><p>You don't have permission to access to this URL on this server.</p>");
+            if (status == http::STATUS_CODE::BAD_REQUEST)
+                return generate_html("400 Bad Request", "<h3>400 error</h3><p>You request is malformed.</p>");
+            return "";
+        }
+    };
+} // namespace misc
