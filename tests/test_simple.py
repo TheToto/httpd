b1ff069eac0adf5a64e60c50b575de7635a6eb0e
Geraud Magne <geraud.magne@epita.fr>
2019-02-27 20:31:20 +0100

    [TESTS] basic working architecture of test_simple.py

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -5,23 +5,25 @@
 import pytest
 import requests
 import subprocess
+import os
+import signal
 
 # must be present in a function "test_*" ; You can write multiple functions
 def test_simple():
 
     # this is the server process
     #              change json location here ->
-    serverProc = subprocess.Popen(["./spider", "tests/json/test1.json"],
+    serverProc = subprocess.Popen(["./spider tests/json/test1.json"],
                                   shell=True, preexec_fn=os.setsid)
 
     # for example, a simple get request to tests/test.page
     # you can do way more varied tests with the Requests library
 
-    requ = requests.get('127.0.0.1:8000/tests/test.page')
-    expectedStr = open("tests/test/page", "r").read()
+    requ = requests.get('http://127.0.0.1:8000/tests/test.page')
+    expectedStr = open("tests/test.page", "r").read()
 
     # write tests as assertions here
     assert(requ.text == expectedStr)
 
     # use this command to kill the server
-    os.killpg(os.getpgid(pro.pid), signal.SIGTERM)
+    os.killpg(os.getpgid(serverProc.pid), signal.SIGTERM)
