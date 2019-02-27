569efac993b2922a80aa3ef145252f8d6dd5153f
Geraud Magne <geraud.magne@epita.fr>
2019-02-27 19:28:03 +0100

    [TESTS] fixed basic environnement issue

--- tests_run.sh
+++ tests_run.sh
@@ -2,11 +2,10 @@
 
 if test ! -d env ; then
     python -m venv env
-fi
-
     source env/bin/activate
-if test ! -d env ; then
     pip install -r requirements.txt
+else
+    source env/bin/activate
 fi
 
 pytest -vv
