f44789ab3c79f11d812e0fe3f4c26df6ae0b2013
Louis HOLLEVILLE <louis.holleville@epita.fr>
2019-03-01 00:42:10 +0100

    [TESTS] Added simple method tests

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -11,7 +11,7 @@ import time
 from spider_misc import *
 
 # must be present in a function "test_*" ; You can write multiple functions
-def test_simple():
+def test_get_simple():
 
     # this is the server process
     #              change json location here ->
@@ -29,4 +29,18 @@ def test_simple():
     assert(requ.headers['content-length'] == str(len(expectedStr)))
 
     # use this command to kill the server
+    kill_server(serverProc)
+
+def test_head_simple():
+    serverProc = launch_server()
+    assert(serverProc != None)
+    resp = requests.head('http://127.0.0.1:8000')
+    assert(resp.status_code == 200)
+    kill_server(serverProc)
+
+def test_post_simple():
+    serverProc = launch_server()
+    assert(serverProc != None)
+    resp = requests.post('http://127.0.0.1:8000')
+    assert(resp.status_code == 200)
     kill_server(serverProc)
