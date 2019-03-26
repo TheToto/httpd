c998f9888d268b7bf9973c7e7870f72447ab1157
Thomas Lupin <thomas.lupin@epita.fr>
2019-03-26 12:15:20 +0100

    [FOR SNATHAN] Sorry my friend, base64 is in provided files.

--- src/config/base64.hh
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <string>
-
-std::string to_64(const std::string& target);
--- src/config/config.cc
+++ src/config/config.cc
@@ -9,16 +9,17 @@
 #ifdef __clang__
 #    pragma GCC diagnostic ignored "-Wlogical-op-parentheses"
 #endif
+#pragma GCC diagnostic pop
 #include <fstream> //ifstream
 #include <iostream> //clog
 #include <json.hpp>
+#include <list>
+#include <optional>
+#include <set>
 #include <stdexcept> //invalid_argument
 #include <string> //to_string
-#include <list>
-#include <set>
-#include <optional>
-#include "base64.hh"
-#pragma GCC diagnostic pop
+
+#include "misc/openssl/base64.hh"
 
 using json = nlohmann::json;
 
@@ -32,14 +33,15 @@ namespace http
         std::string tmp2;
         try
         {
-
             for (auto i : proxy["proxy_set_header"].items())
             {
                 tmp = i.value();
                 tmp2 = i.key();
-                proxy_set_header[tmp2] =  tmp;
+                proxy_set_header[tmp2] = tmp;
             }
-        } catch(const std::exception&) {}
+        }
+        catch (const std::exception&)
+        {}
         try
         {
             for (auto i : proxy["proxy_remove_header"])
@@ -47,7 +49,9 @@ namespace http
                 tmp = i;
                 proxy_remove_header.insert(tmp);
             }
-        } catch(const std::exception&) {}
+        }
+        catch (const std::exception&)
+        {}
         try
         {
             for (auto i : proxy["set_header"].items())
@@ -56,7 +60,9 @@ namespace http
                 tmp2 = i.key();
                 set_header[tmp2] = tmp;
             }
-        } catch(const std::exception&) {}
+        }
+        catch (const std::exception&)
+        {}
         try
         {
             for (auto i : proxy["remove_header"])
@@ -64,8 +70,9 @@ namespace http
                 tmp = i;
                 remove_header.insert(tmp);
             }
-
-        } catch(const std::exception&) {}
+        }
+        catch (const std::exception&)
+        {}
 
         ipv6_ = "[" + ip_ + "]";
         ip_port_ = ip_ + ":" + std::to_string(port_);
@@ -73,14 +80,24 @@ namespace http
     }
 
     VHostConfig::VHostConfig(std::string ip, int port, std::string server_name,
-            std::string root, std::string def, std::string sslc,
-            std::string sslk, std::optional<ProxyConfig> proxy,
-            std::string authb, std::list<std::string> authbu,
-            std::string health, bool autoi, bool def_vh):
-        ip_(ip), port_(port), server_name_(server_name), root_(root),
-        default_file_(def), ssl_cert_(sslc), ssl_key_(sslk), proxy_pass_(proxy),
-        auth_basic_(authb), auth_basic_users_(authbu), health_endpoint_(health),
-        auto_index_(autoi), default_vhost_(def_vh)
+                             std::string root, std::string def,
+                             std::string sslc, std::string sslk,
+                             std::optional<ProxyConfig> proxy,
+                             std::string authb, std::list<std::string> authbu,
+                             std::string health, bool autoi, bool def_vh)
+        : ip_(ip)
+        , port_(port)
+        , server_name_(server_name)
+        , root_(root)
+        , default_file_(def)
+        , ssl_cert_(sslc)
+        , ssl_key_(sslk)
+        , proxy_pass_(proxy)
+        , auth_basic_(authb)
+        , auth_basic_users_(authbu)
+        , health_endpoint_(health)
+        , auto_index_(autoi)
+        , default_vhost_(def_vh)
     {
         if (!inet_pton(AF_INET, ip_.c_str(), &addr.sin_addr))
         {
@@ -103,89 +120,110 @@ namespace http
         std::string server_name = i["server_name"];
 
         if (ip.empty() || port <= 0 || server_name.empty())
-            throw std::invalid_argument("invalid JSON file: a mandatory \
-argument is missing");
+            throw std::invalid_argument("invalid JSON file: a mandatory "
+                                        "argument is missing");
 
         std::string root;
         try
         {
             root = i["root"];
-        } catch (const std::exception& e){
+        }
+        catch (const std::exception& e)
+        {
             root = ".";
         }
         std::string default_file;
         try
         {
             default_file = i["default_file"];
-        } catch (const std::exception& e){
+        }
+        catch (const std::exception& e)
+        {
             default_file = "index.html";
         }
         std::string ssl_cert = "";
         try
         {
             ssl_cert = i["ssl_cert"];
-        } catch (const std::exception& e){}
+        }
+        catch (const std::exception& e)
+        {}
 
         std::string ssl_key = "";
         try
         {
             ssl_key = i["ssl_key"];
-        } catch (const std::exception& e){}
+        }
+        catch (const std::exception& e)
+        {}
         if (ssl_key.empty() != ssl_cert.empty())
-            throw std::invalid_argument("ERROR: ssl_cert and ssl_key must \
-be defined simulteanously");
+            throw std::invalid_argument("ERROR: ssl_cert and ssl_key must "
+                                        "be defined simulteanously");
 
         std::string auth_basic = "";
         try
         {
             auth_basic = i["auth_basic"];
-        } catch (const std::exception& e){}
+        }
+        catch (const std::exception& e)
+        {}
 
         std::list<std::string> auth_basic_users;
         try
         {
             for (std::string cur : json(i["auth_basic_users"]))
-                auth_basic_users.push_front(to_64(cur));
-        } catch (const std::exception& e){}
+                auth_basic_users.push_front(ssl::base64_encode(cur));
+        }
+        catch (const std::exception& e)
+        {}
 
         if (auth_basic_users.empty() != auth_basic.empty())
-            throw std::invalid_argument("ERROR: auth_basic and auth_basic_users\
- must be defined simulteanously");
+            throw std::invalid_argument("ERROR: auth_basic and auth_basic_users"
+                                        " must be defined simulteanously");
 
         std::string health_endpoint = "";
         try
         {
             health_endpoint = i["auth_endpoint"];
-        } catch (const std::exception& e){}
+        }
+        catch (const std::exception& e)
+        {}
 
         bool auto_index = false;
         try
         {
-             auto_index = i["auto_index"];
-        } catch (const std::exception& e){}
+            auto_index = i["auto_index"];
+        }
+        catch (const std::exception& e)
+        {}
 
         bool default_vhost = false;
         try
         {
-             default_vhost = i["default_vhost"];
-        } catch (const std::exception& e){}
-
+            default_vhost = i["default_vhost"];
+        }
+        catch (const std::exception& e)
+        {}
 
         std::optional<ProxyConfig> proxy = std::nullopt;
         try
         {
             proxy = ProxyConfig(i["proxy_pass"]);
+        }
+        catch (const std::exception& e)
+        {}
 
-        } catch (const std::exception& e){}
+        if ((proxy != std::nullopt)
+            && (root != "." || default_file != "index.html"
+                || auto_index != false || default_vhost != false))
+            throw std::invalid_argument(
+                "ERROR: a proxy_pass cannot be set if "
+                "root/defailt_file/auto_index/default_vhost is set");
 
-        if ((proxy != std::nullopt) && (root != "." || default_file != "index.html"
-                    || auto_index != false || default_vhost != false))
-            throw std::invalid_argument("ERROR: a proxy_pass cannot be set if \
-root/defailt_file/auto_index/default_vhost is set");
-
-        serv.VHosts_.push_back(VHostConfig(ip, port, server_name, root,
-                default_file, ssl_cert, ssl_key, proxy, auth_basic,
-                auth_basic_users, health_endpoint, auto_index, default_vhost));
+        serv.VHosts_.push_back(
+            VHostConfig(ip, port, server_name, root, default_file, ssl_cert,
+                        ssl_key, proxy, auth_basic, auth_basic_users,
+                        health_endpoint, auto_index, default_vhost));
     }
 
     ServerConfig parse_configuration(const std::string& path)
@@ -199,15 +237,21 @@ root/defailt_file/auto_index/default_vhost is set");
         try
         {
             serv.payload_max_size = j["payload_max_size"];
-        } catch(const std::exception&){}
+        }
+        catch (const std::exception&)
+        {}
         try
         {
             serv.uri_max_size = j["uri_max_size"];
-        } catch(const std::exception&){}
+        }
+        catch (const std::exception&)
+        {}
         try
         {
             serv.header_max_size = j["header_max_size"];
-        } catch(const std::exception&){}
+        }
+        catch (const std::exception&)
+        {}
 
         auto ctn = j["vhosts"];
 
@@ -237,8 +281,8 @@ root/defailt_file/auto_index/default_vhost is set");
             json tmp(i);
             if (tmp.size() != 4 && tmp.size() != 5)
             {
-                std::clog << ("invalid json file: not a \
-                        json or wrong architecture\n");
+                std::clog << ("invalid json file: not a "
+                              "json or wrong architecture\n");
                 return 1;
             }
             std::string ip = tmp["ip"];
@@ -257,8 +301,8 @@ root/defailt_file/auto_index/default_vhost is set");
 
             if (ip.empty() || port <= 0 || server_name.empty() || root.empty())
             {
-                std::clog << ("invalid JSON file: a mandatory \
-                        argument is missing\n");
+                std::clog << ("invalid JSON file: a mandatory "
+                              "argument is missing\n");
                 return 1;
             }
         }
