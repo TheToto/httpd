761103b940aa44f25e17dcff41a22e55a68e0b0b
Thetoto <thomas.lupin@epita.fr>
2019-05-01 15:17:19 +0200

    [THREAD] If nb_worker = 1, dont fork (for easy debug)

--- src/main.cc
+++ src/main.cc
@@ -39,6 +39,29 @@ static void stop_server(struct ev_loop* loop, ev_signal*, int)
 static void continue_server(struct ev_loop*, ev_signal*, int)
 {}
 
+static int launch_thread(int i)
+{
+    for (auto conf : http::serv_conf.VHosts_)
+    {
+        std::clog << "Setup " << conf.server_name_ << " vhost.\n";
+        http::VHostFactory::Create(conf);
+    }
+
+    auto loop = http::event_register.loop_get();
+
+    ev_signal sigint_watcher;
+    ev_signal_init(&sigint_watcher, stop_server, SIGINT);
+    loop.register_sigint_watcher(&sigint_watcher);
+
+    ev_signal sigpipe_watcher;
+    ev_signal_init(&sigpipe_watcher, continue_server, SIGPIPE);
+    loop.register_sigint_watcher(&sigpipe_watcher);
+
+    std::clog << "Server launched ! Process " << i << std::endl;
+    loop();
+    return 0;
+}
+
 static int launch_server(char* path)
 {
     http::serv_conf = http::parse_configuration(path);
@@ -48,31 +71,16 @@ static int launch_server(char* path)
 
     std::vector<int> pids;
 
+    if (http::serv_conf.nb_workers == 1)
+        return launch_thread(-1);
+
     for (int i = 0; i < http::serv_conf.nb_workers; i++)
     {
         int pid = fork();
 
         if (pid == 0)
         {
-            for (auto conf : http::serv_conf.VHosts_)
-            {
-                std::clog << "Setup " << conf.server_name_ << " vhost.\n";
-                http::VHostFactory::Create(conf);
-            }
-
-            auto loop = http::event_register.loop_get();
-
-            ev_signal sigint_watcher;
-            ev_signal_init(&sigint_watcher, stop_server, SIGINT);
-            loop.register_sigint_watcher(&sigint_watcher);
-
-            ev_signal sigpipe_watcher;
-            ev_signal_init(&sigpipe_watcher, continue_server, SIGPIPE);
-            loop.register_sigint_watcher(&sigpipe_watcher);
-
-            std::clog << "Server launched ! Process " << i << std::endl;
-            loop();
-            return 0;
+            return launch_thread(i);
         }
         else
         {
