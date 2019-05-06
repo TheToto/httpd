61cd81c183384b06c006522852add07573c13b37
Thetoto <thomas.lupin@epita.fr>
2019-05-06 23:55:45 +0200

    [TRASH] Remove trash files

--- change.sh
+++
@@ -1,8 +0,0 @@
-git filter-branch --commit-filter \
-'if [ "$GIT_AUTHOR_NAME" = "Thetoto" ]; then \
-export GIT_AUTHOR_NAME="Thomas Lupin";\
-export GIT_AUTHOR_EMAIL=thomas.lupin@epita.fr;\
-export GIT_COMMITTER_NAME="Thomas Lupin";\
-export GIT_COMMITTER_EMAIL=thomas.lupin@epita.fr;\
-fi;\
-git commit-tree "$@"'
